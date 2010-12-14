b2e9e5f94950aa6d968c68381df288b756546865
cuchac <cuchac@email.cz>
2010-12-14 21:46:58 +0100

    Use new Django function isAjax()

--- bm/client/context_processors.py
+++ bm/client/context_processors.py
@@ -2,7 +2,7 @@ from bm.client.menu import Menu
 from bm.client.views import MENU
 
 def menu(request):
-    if request.path[:6] == "/ajax/":
+    if request.is_ajax():
         return {}
     
     return {'menu' : Menu(MENU)}
