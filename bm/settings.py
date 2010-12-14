a3b381705afb7eda7ea0e01762510e43fd99fb0c
cuchac <cuchac@email.cz>
2010-12-14 19:51:49 +0100

    Disable Django Dev Toolbar

--- bm/settings.py
+++ bm/settings.py
@@ -107,5 +107,5 @@ INSTALLED_APPS = (
 
 INTERNAL_IPS = ('127.0.0.1',)
 
-INSTALLED_APPS = INSTALLED_APPS+('debug_toolbar',)
-MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+#INSTALLED_APPS = INSTALLED_APPS+('debug_toolbar',)
+#MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + ('debug_toolbar.middleware.DebugToolbarMiddleware',)
